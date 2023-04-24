1fa5c19d8d023dc7a9dba621545eae6300d09498
Adrian L Lange <git@p3lim.net>
2023-04-24 17:46:58 +0200

    Add option to exclude words from author section

--- update.py
+++ update.py
@@ -13,6 +13,7 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument('-f', '--files', help='Multiline string of file paths', required=True)
+parser.add_argument('-e', '--exclude', help='Multiline string of words in author to exclude')
 args = parser.parse_args()
 
 paths = []
@@ -27,12 +28,21 @@ for line in iter(args.files.splitlines()):
 		else:
 			paths.append(line)
 
+excludes = []
+for line in iter((args.exclude or '').splitlines()):
+	if line != '':
+		if line in excludes:
+			print(f'duplicate exclude "{line}"')
+			sys.exit(1)
+		else:
+			excludes.append(line.lower())
+
 PATTERN = re.compile(r'^([Cc]opyright.+) ([0-9-]{4,9}) (.+)$')
 NEW_YEAR = datetime.now().year
 
 for line in fileinput.input(files=paths, inplace=True):
 	match = PATTERN.match(line)
-	if match and match.group(2) != str(NEW_YEAR):
+	if match and match.group(2) != str(NEW_YEAR) and not any(w in match.group(3).lower() for w in excludes):
 		sys.stdout.write(f'{match.group(1)} {match.group(2)[:4]}-{NEW_YEAR} {match.group(3)}\n')
 	else:
 		sys.stdout.write(line)
