7f7010c73e24c998e840a257ae46922aafb71546
Adrian L Lange <git@p3lim.net>
2023-04-24 17:40:10 +0200

    Use argparse to handle args, moving input file paths to an option as multiline strings

--- update.py
+++ update.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
 
+import os
 import re
 import sys
 import fileinput
+import argparse
 from datetime import datetime
 
-if len(sys.argv) <= 1:
-	print('Please tell me which license file(s) to update')
-	sys.exit(1)
+parser = argparse.ArgumentParser(
+	usage='%(prog)s [OPTION]',
+	description='Updates license file(s) to the current year.'
+)
+
+parser.add_argument('-f', '--files', help='Multiline string of file paths', required=True)
+args = parser.parse_args()
+
+paths = []
+for line in iter(args.files.splitlines()):
+	if line != '':
+		if not os.path.isfile(line):
+			print(f'file not found at path "{line}"')
+			sys.exit(1)
+		elif line in paths:
+			print(f'duplicate path "{line}"')
+			sys.exit(1)
+		else:
+			paths.append(line)
 
 PATTERN = re.compile(r'^([Cc]opyright.+) ([0-9-]{4,9}) (.+)$')
 NEW_YEAR = datetime.now().year
 
-for line in fileinput.input(inplace=True):
+for line in fileinput.input(files=paths, inplace=True):
 	match = PATTERN.match(line)
 	if match:
 		sys.stdout.write(f'{match.group(1)} {match.group(2)[:4]}-{NEW_YEAR} {match.group(3)}\n')
